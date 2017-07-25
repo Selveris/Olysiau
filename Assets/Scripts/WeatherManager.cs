4c56d6493423c72734d0eaaa061c3bc04c542973
Gabriel Xavier Luisier <gabriel.luisier@epfl.ch>
2017-07-25 17:41:44 +0200

    Create WeatherZone

---
+++ Assets/Scripts/WeatherManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour {
+
+    public GameObject sun;
+    public GameObject rainingCloud;
+
+    private bool raining;
+    private Transform weatherSpawn;
+    private GameObject lastInst;
+
+	// Use this for initialization
+	void Start () {
+        weatherSpawn = transform.Find("WeatherSpawn");
+        raining = false;
+        set_sun();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    bool isRaining()
+    {
+        return raining;
+    }
+
+    void set_rain()
+    {
+        raining = true;
+        instantiateWeather(rainingCloud);
+    }
+
+    void set_sun()
+    {
+        raining = false;
+        instantiateWeather(sun);
+    }
+
+    private void instantiateWeather(GameObject o)
+    {
+        Destroy(lastInst);
+        lastInst = Instantiate<GameObject>(o, weatherSpawn);
+        lastInst.transform.SetParent(transform, true);
+    }
+}
