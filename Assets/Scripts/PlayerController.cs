c317c8490ea5e4362f356af26e654c798ac74d7e
Quillasp <saralfddin93@yahoo.fr>
2017-07-26 14:10:54 +0200

    Some corrections in the player controller script.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,40 +9,29 @@ public class PlayerController : MonoBehaviour {
     public GameObject objectSun;
     public GameObject objectRain;
 
-    private float speed = 7;
-    private float forceJump = 2.58f;
+    private float speed = 7;                // Set the player max speed
+    private float forceJump = 2.58f;        // Set the play jumping force
     private Rigidbody2D myBeautifulBody;
-    private Vector2 startUpVelocity;
-    private bool jumping = false;
-    private bool isGrounded;
-    private GameObject[] spawners;
-    private WeatherManager weatherZone;
+    private Vector2 startUpVelocity;        // Get the player velocity on the start to prevent weird Y axis behavior
+    private bool jumping = false;           // Set the player jump at false so he can jump when grounded
+    private bool isGrounded;                // Set the player grounded status at false so he can jump when grounded
+    private WeatherManager weatherManager;     // Getting the WeatherManager from the weatherZone gameObject
 
 	// Use this for initialization
 	void Start () {
         myBeautifulBody = GetComponent<Rigidbody2D>();
-        spawners = GameObject.FindGameObjectsWithTag("Spawner");
-        for (int i = 0; i < spawners.Length; i++) {
-            print(spawners[i]);
-        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
         startUpVelocity = myBeautifulBody.velocity;
-
         isGrounded = Physics2D.OverlapCircle(pointStartRayCast.position, 0.2f, layer.value);
 
         MovingPlayerX();
         Jump();
 	}
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-		if (collision.transform.tag == "Ground") {
-			isGrounded = true;
-			jumping = false;
-		}
-    }
+    // Input from the player to move itself to the right or left and a jumping function
 
     private void MovingPlayerX() {
         float direction = Input.GetAxis("Horizontal");
@@ -61,21 +50,35 @@ public class PlayerController : MonoBehaviour {
 		else jumping &= !isGrounded;
     }
 
+    // Detect when the player is in contact with the ground (gameObject tagged with "Ground")
+	private void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.transform.tag == "Ground") {
+			isGrounded = true;
+			jumping = false;
+		}
+	}
+
+    // Detect when the player is not in contact with ground anymore
     private void OnCollisionExit2D(Collision2D collision) {
         if (collision.transform.tag == "Ground")
             isGrounded = false;
     }
 
+    // Detect when the player enter in a weather zone and get its WeatherManager script
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "WeatherZone")
-            weatherZone = collision.gameObject.GetComponent<WeatherManager>();
+            weatherManager = collision.gameObject.GetComponent<WeatherManager>();
     }
 
+    // Player keyboard input to lauch a weather type (f -> invoke sun, r -> invoke rain)
+
     private void Sun() {
-        weatherZone.set_sun();
+        if (Input.GetKey("f"))
+            weatherManager.set_sun();
     }
 
     private void Rain() {
-        weatherZone.set_rain();
+        if (Input.GetKey("r"))
+            weatherManager.set_rain();
     }
 }
