ef6d5abb55c679b1b567592bbcb901ac3ad55677
Loris Witschard <loris.wit@gmail.com>
2017-07-27 16:47:28 +0200

    Create main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{	
+	public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+	}
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        if (Application.isEditor)
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
