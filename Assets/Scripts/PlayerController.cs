e4e1cedfe9babdaee4c39ed52add3d008b5f9fc6
Quillasp <saralfddin93@yahoo.fr>
2017-07-27 17:11:09 +0200

    Adding of the player animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,8 +3,6 @@
 public class PlayerController : MonoBehaviour {
     
     public LayerMask layer;
-	public Sprite walkingSprite;
-	public Sprite dancingSprite;
 
     private float speed = 7;                        // Set the player max speed
     private float forceJump = 2.58f;                // Set the play jumping force
@@ -18,6 +16,7 @@ public class PlayerController : MonoBehaviour {
     private float cameraTransitionSpeed = 7;        // Setting the translation speed between the WeatherZone
     private bool danceMode;                         // Setting the player mode (normal mode and danse mode)
     private SpriteRenderer playerSpriteRenderer;    // Get the player SpriteRenderer to modify the sprite ingame
+    private Animator playerAnimator;
 
 	// Use this for initialization
 	void Start () {
@@ -25,7 +24,7 @@ public class PlayerController : MonoBehaviour {
         playerSpriteRenderer = GetComponent<SpriteRenderer>();
         camera = GameObject.Find("Main Camera").GetComponent<Camera>();
         danceMode = false;
-        // transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+        playerAnimator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -58,8 +57,9 @@ public class PlayerController : MonoBehaviour {
 
     private void MovePlayerX() {
         float direction = Input.GetAxis("Horizontal");
+        bool isMoving = Mathf.Abs(direction) > 0.001f;
+        playerAnimator.SetBool("isMoving", isMoving);
         playerRigidBody.velocity = new Vector2(direction * speed, startUpVelocity.y);
-        playerSpriteRenderer.sprite = walkingSprite;
         if (direction > 0) {
             FlipSprite(0);
         } else if (direction < 0) {
@@ -82,11 +82,11 @@ public class PlayerController : MonoBehaviour {
 
 	private void DanceMode() {
 		if (Input.GetKeyDown(KeyCode.LeftControl)) {
-            playerSpriteRenderer.sprite = dancingSprite;
 			if (!danceMode)
 				danceMode = true;
 			else
 				danceMode = false;
+            playerAnimator.SetBool("isDancing", danceMode);
 		}
 	}
 
