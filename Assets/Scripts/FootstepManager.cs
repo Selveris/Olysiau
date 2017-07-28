7e550870472df3ad5e1e7762ba29361282f53b73
Quillasp <saralfddin93@yahoo.fr>
2017-07-28 02:12:32 +0200

    Finalisation of the PlayerController

---
+++ Assets/Scripts/FootstepManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepManager : MonoBehaviour {
+
+    private AudioClip[] footsteps;
+
+	// Use this for initialization
+	void Start () {
+        footsteps = Resources.LoadAll<AudioClip>("Sounds/FXs/Footsteps/");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayFootstep () {
+        AudioClip footstep = footsteps[Random.Range(0, footsteps.Length - 1)];
+        GetComponent<AudioSource>().PlayOneShot(footstep);
+    }
+}
