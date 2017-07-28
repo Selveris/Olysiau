5a2068ae501f985ff95fba999a41869d9bfe9c6b
Quillasp <saralfddin93@yahoo.fr>
2017-07-28 08:17:28 +0200

    Adding player shouts and gulps

---
+++ Assets/Scripts/EatingNoiseManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatingNoiseManager : MonoBehaviour {
+
+    private AudioClip[] eatings;
+
+	// Use this for initialization
+	void Start () {
+		eatings = Resources.LoadAll<AudioClip>("Sounds/FXs/EatPlant/");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayEatings()
+	{
+		AudioClip eating = eatings[Random.Range(0, eatings.Length - 1)];
+		GetComponent<AudioSource>().PlayOneShot(eating);
+	}
+}
